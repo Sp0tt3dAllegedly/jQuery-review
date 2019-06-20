2d848a86b0bfabbdc0d0011f100b19b5d86ebd88
Jake Knecht <Ljknecht91@gmail.com>
2019-06-20 15:19:11 -0500

    added clear task list and flag task buttons

--- JS/cilantro.js
+++ JS/cilantro.js
@@ -4,7 +4,12 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('woot');
-    $('#add-task').on('click', addTaskHandler)
+    $('#add-task').on('click', addTaskHandler);
+    $('#task-list').on('click', '.delete', deleteTask);
+    $('#task-list').on('click', '.flag', flagTask);
+    $('#task-list').on('click', '.clear', deleteAllTasks);
+    $('#clear-tasks').on('click', deleteAllTasks);
+    
 //end onReady
 }
 
@@ -32,6 +37,39 @@ renderTask(taskName);
 
 function renderTask(task) {
     console.log('in addTask', task);
-    $('#task-list').append(`<li>${task}</li>`);
+    $('#task-list').append(
+        `<li>
+    ${task}
+    <button class = "delete">Delete</button>
+    <button class = "flag">Flag</button>
+    </li>`);
+    
+   
 }
 
+function deleteTask(event) {
+    console.log('clicked delete button', $(this).parent().text());
+// 'this' is the delete button...
+// We want to remove it's parent NOT parentS; if you include the s, the ENTIRE PAGE will be deleted.
+
+   $(this).parent().remove();
+    
+}
+
+function flagTask(event) {
+    console.log('clicked flag as important', $(this).parent().text());
+// made a style class in css .important
+// we want to select the <li> which is the parent
+// of 'this' of the thing we clicked on
+// make changes to css via css file, not inline styling - this is not industry standard any longer
+
+$(this).addClass('important'); //can also use $(this).parent().addClass('important'); to add to parent item
+        // above method only changes color of flag button
+}
+
+
+// this will remove all tasks from the list when clicked
+
+function deleteAllTasks(event){
+    $('#task-list').empty();
+}
